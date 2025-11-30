ce03083468e13bea0d3aa2f317e480d686b5d3ae
rahmann9 <rahmann9@bxscience.edu>
2025-11-30 14:17:31 -0500

    Complete Intro to HTML course
    - Finished all lessons and exercises:
      HTML tags, <img> and <a> tags, nesting, buttons, inputs, lists
    - Built hands-on projects: BBC news edit, Google build, personal website

--- 02. HTML and CSS Fundamentals/01. Intro to HTML/13. Build a Personal Website/index.js
+++ 02. HTML and CSS Fundamentals/01. Intro to HTML/13. Build a Personal Website/index.js
@@ -2,10 +2,10 @@
 // Replace the arguments below according to your preference.
 
 // space, scary, military, romantic, cowboy, fantasy, superhero
-favouriteMovieGenre("regular")
+favouriteMovieGenre("scary")
 
 // watermelon, tomato, banana, orange, avocado, blueberry
-favouriteFruit("regular")
+favouriteFruit("blueberry")
 
 // light, dark
 favouriteMode("light")
